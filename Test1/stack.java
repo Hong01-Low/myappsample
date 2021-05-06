b49fd1f7a45591a61fb85372f4c5ff18e3cfafc2
Chun Hong <junhong.low123@gmail.com>
2021-05-06 12:03:43 +0800

    I only want put all program in one file

---
+++ Test1/stack.java
@@ -0,0 +1,23 @@
+import java.util.Stack;
+
+class Main {
+    public static void main(String[] args) {
+        Stack<String> animals = new Stack<>();
+        // add elements to Stack//
+
+        animals.push("Elephant");
+        animals.push("Cat");
+        animals.push("Tiger");
+        animals.push("Lion");
+        animals.push("Snake");
+        System.out.println("Stack : " + animals);
+
+        // Remove the elements of in the Stack//
+        String elements = animals.pop();
+        System.out.println("Remove the element : " + elements);
+
+        // access elements from the top//
+        String element = animals.peek();
+        System.out.println("Animals at top : " + element);
+    }
+}
